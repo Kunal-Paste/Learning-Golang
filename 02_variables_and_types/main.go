14b3b50442656a8cc1821e0709a835fc9c0b4c62
Kunal-Paste <pastekunal6@gmail.com>
2026-05-05 13:44:18 +0530

    add initial implementation of main function with variable declarations and print statements

---
+++ 02_variables_and_types/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var channelName string
+	channelName = "AI-production"
+
+	var year int
+	year = 2026
+
+	fmt.Println("channel:", channelName)
+	fmt.Println("year:", year)
+
+	var rating float64 = 4.7
+	fmt.Println("rating:", rating)
+
+}
---
+++ 03_packages_and_import/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("squareroot of 25: ", math.Sqrt(25))
+}
